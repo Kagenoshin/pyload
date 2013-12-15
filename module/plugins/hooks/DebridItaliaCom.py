ae7dd21c541d035e61fa806a22c2c24601cca759
Ivo Buff <kagenoshin@gmx.ch>
2013-12-15 16:59:45 +0100

    DebridItaliaCOM hook update
    import getURL

--- module/plugins/hooks/DebridItaliaCom.py
+++ module/plugins/hooks/DebridItaliaCom.py
@@ -15,6 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.    #
 ############################################################################
 
+from module.network.RequestFactory import getURL
 from module.plugins.internal.MultiHoster import MultiHoster
 
 
